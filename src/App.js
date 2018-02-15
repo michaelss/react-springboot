f0f796558f3b759b64250db72da2e71cccd1987e
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-15 17:05:49 -0300

    Criação do componente Cabeçalho.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './componentes/Cabecalho';
 
 class App extends Component {
 
@@ -28,10 +28,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Cabecalho titulo="Primeira aplicação com React" />
         <p className="App-intro">
           {this.state.nome} - {this.state.email}
           <br/>
