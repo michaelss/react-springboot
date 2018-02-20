654a2452053ab51546c70c8f7650060d3fbad867
Michael Schuenck dos Santos <michael@tre-to.jus.br>
2018-02-20 14:26:38 -0300

    Integrando frontend e backend.

--- frontend/src/componentes/funcionario/ListaFuncionario.js
+++ frontend/src/componentes/funcionario/ListaFuncionario.js
@@ -2,9 +2,57 @@ import React, { Component } from 'react';
 
 export default class ListaFuncionario extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            funcionarios: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('/funcionarios')
+            .then(resp => {
+                if (resp.status === 200) {
+                    return resp.json()
+                }
+                return []
+            })
+            .then(json => this.setState({funcionarios: json}))
+    }
+
+    enviar(e) {
+        e.preventDefault();
+
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({nome: 'XYZ', email: 'xyz@gmail.com'}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch('/funcionarios', requestInfo)
+            .then(response => {
+                if (response.status === 200) {
+                    this.setState({msg: 'Inserido'});
+                }
+            })
+            .catch(erro => {
+                this.setState({msg: 'Erro'});
+            });
+    }
+
     render() {
         return (
-            <h1>Lista de Funcionários</h1>
+            <div>
+                <h1>Lista de Funcionários</h1>
+                <ul>
+                    {this.state.funcionarios.map(f => <li>{f.nome}</li>)}
+                </ul>
+                <form onSubmit={this.enviar.bind(this)}>
+                    <button type="submit">Cadastrar</button>
+                </form>
+            </div>
         );
     }
 
